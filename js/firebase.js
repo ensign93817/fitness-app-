1fb299faa61f3e266497d25b3c01efd586309223
ensign93817 <58337851+ensign93817@users.noreply.github.com>
2025-10-26 14:07:54 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,38 @@
-import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+// js/firebase.js (ESM)
+// 以 ESM 方式透過官方 CDN 匯入模組
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getFirestore, collection, doc, getDoc, setDoc, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBurBoRPT0csPqtyDSOQBYMj1Gaqf3EB0",
-  authDomain: "fitness-guide-9a8f3.firebaseapp.com",
-  projectId: "fitness-guide-9a8f3",
-  storageBucket: "fitness-guide-9a8f3.appspot.com",
-  messagingSenderId: "969288112649",
-  appId: "1:969288112649:web:58b5b807c410388b1836d8",
-  measurementId: "G-7XL1324K8Q"
+
+// TODO: 替換成你的 Firebase 專案設定
+export const firebaseConfig = {
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+projectId: "YOUR_PROJECT_ID",
+storageBucket: "YOUR_PROJECT_ID.appspot.com",
+messagingSenderId: "000000000000",
+appId: "1:000000000000:web:xxxxxxxxxxxxxxxx"
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-export const db = getFirestore(app);
+
+let _app=null, _db=null;
+export function getDb(){
+if(!_app){ _app = initializeApp(firebaseConfig); }
+if(!_db){ _db = getFirestore(_app); }
+return _db;
+}
+
+
+// 工具: 取得/建立本地 uid（無登入情境）
+export function getLocalUID(){
+let uid = localStorage.getItem('uid');
+if(!uid){
+uid = crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).slice(2);
+localStorage.setItem('uid', uid);
+}
+return uid;
+}
+
+
+// 匯出 Firestore 常用 API（方便其他檔案使用）
+export { collection, doc, getDoc, setDoc, addDoc, getDocs, query, where };
