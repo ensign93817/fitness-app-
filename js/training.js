8c5c94199a2231d7a40826f7aaf58bb47a69055d
ensign93817 <58337851+ensign93817@users.noreply.github.com>
2025-10-28 03:27:26 +0800

    Update training.js

--- js/training.js
+++ js/training.js
@@ -255,6 +255,7 @@ for (const { name, chart } of charts) {
 alert(`✅ 今日訓練總重量：${totalToday.toFixed(1)} kg 已儲存！`);
 // === 頁面載入後執行 ===
 // === 頁面載入後執行 ===
+// === 頁面載入後執行 ===
 window.addEventListener("DOMContentLoaded", () => {
   // 1️⃣ 檢查使用者名稱是否存在
   let userName = localStorage.getItem("userName");
