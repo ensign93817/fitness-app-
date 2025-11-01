105f747ef8720fef2c64f088a8b3e57e3a43f185
ensign93817 <58337851+ensign93817@users.noreply.github.com>
2025-11-02 04:12:36 +0800

    Update training.js

--- js/training.js
+++ js/training.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBur0DoRPT0csPqtyDSOQBYMjlGaqf3EB0",
   authDomain: "fitness-guide-9a8f3.firebaseapp.com",
   projectId: "fitness-guide-9a8f3",
-  storageBucket: "fitness-guide-9a8f3.appspot.com", // ✅ 改正錯字 firebasestorage.app → appspot.com
+  storageBucket: "fitness-guide-9a8f3.firebasestorage.app",
   messagingSenderId: "969288112649",
   appId: "1:969288112649:web:58b5b807c410388b1836d8",
   measurementId: "G-7X1L324K0Q"
