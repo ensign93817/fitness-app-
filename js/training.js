f1b2549a50afc19b24039d4cfeffa149463eb808
ensign93817 <58337851+ensign93817@users.noreply.github.com>
2025-12-02 00:21:42 +0800

    Update training.js

--- js/training.js
+++ js/training.js
@@ -37,6 +37,32 @@ function getMenuDocPart(part) {
   return part;
 }
 
+// =======================
+// 自訂 Confirm（通用）
+// =======================
+function customConfirm(message) {
+  return new Promise(resolve => {
+    const overlay = document.getElementById("confirmOverlay");
+    const msgBox = document.getElementById("confirmMessage");
+    const yesBtn = document.getElementById("confirmYes");
+    const noBtn = document.getElementById("confirmNo");
+
+    msgBox.textContent = message;
+    overlay.style.display = "flex";
+
+    // 是
+    yesBtn.onclick = () => {
+      overlay.style.display = "none";
+      resolve(true);
+    };
+
+    // 否
+    noBtn.onclick = () => {
+      overlay.style.display = "none";
+      resolve(false);
+    };
+  });
+}
 
 
 // === 🔥 Firebase SDK 載入 ===
